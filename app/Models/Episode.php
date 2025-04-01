9b0959f463d7e536d90fe35d3b7e82b5cd5b2afc
Jheorgenes Warlley <jheorgenes@gmail.com>
2025-04-01 16:35:48 -0300

    - Uma forma diferente de fazer o casts de propriedades

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -9,26 +9,28 @@ use Illuminate\Database\Eloquent\Model;
 class Episode extends Model
 {
     use HasFactory;
-
     protected $fillable = ['number'];
     public $timestamps = false;
+    protected $casts = [
+        'watched' => 'boolean'
+    ];
 
     public function season()
     {
         return $this->belongsTo(Season::class);
     }
 
-    /**
-     * 
-     *  Mutators e Castings do atributo watched
-     * 
-     */
-    protected function watched(): Attribute
-    {
-        // Transformando o atributo para sempre retornar booleano mesmo ao invés de um int.
-        return new Attribute(
-            get: fn($watched) => (bool) $watched,
-            set: fn($watched) => (bool) $watched
-        );
-    }
+    // /**
+    //  * 
+    //  *  Mutators e Castings do atributo watched
+    //  * 
+    //  */
+    // protected function watched(): Attribute
+    // {
+    //     // Transformando o atributo para sempre retornar booleano mesmo ao invés de um int.
+    //     return new Attribute(
+    //         get: fn($watched) => (bool) $watched,
+    //         set: fn($watched) => (bool) $watched
+    //     );
+    // }
 }
